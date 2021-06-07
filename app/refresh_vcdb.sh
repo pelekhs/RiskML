51b7ebdce6a10a366a59895a619b2c93500867b8
pelekhs <pelekhs@gmail.com>
2021-06-07 16:18:08 -0700

    - Moved

--- app/refresh_vcdb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-echo "Cloning VCDB ..."
-(git clone https://github.com/vz-risk/VCDB.git temp \
-&& mv temp/.git ../VCDB/.git) \
-|| echo "Database update failed, proceeding with stored VCDB"
-rm -rf temp
--- app/train_n_serve.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-echo "Proceeding to training..."
-./run_app/train_attribute.sh "KNN RF LGBM"
-./run_app/train_asset.sh "LGBM RF KNN"
-./run_app/train_action.sh "LGBM RF KNN" 
-tail -F "KeepingContainerAlive"
