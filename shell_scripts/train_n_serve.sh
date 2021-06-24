47c0ddac8b0b0c3ae522b8982937428e3e5bfe4c
pelekhs <pelekhs@gmail.com>
2021-06-24 07:34:21 -0700

    - Back to cross validation for poor performance

--- shell_scripts/train_n_serve.sh
+++ shell_scripts/train_n_serve.sh
@@ -16,7 +16,7 @@ train_n_serve() {
     then
         for al in $3
         do
-            mlflow run --experiment-name "$modelname" --entry-point train $curdir/.. -P task="$1" -P target="$2" -P train_size=0.85 -P n_folds=5 -P algo="$al" -P split_random_state=0 -P merge=1 --no-conda
+            mlflow run --experiment-name "$modelname" --entry-point train $curdir/.. -P task="$1" -P target="$2" -P train_size=1 -P n_folds=5 -P algo="$al" -P split_random_state=0 -P merge=1 --no-conda
         done
     fi
     if [[ $6 != "--no-serving" ]]
